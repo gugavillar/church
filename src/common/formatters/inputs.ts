9877c95b6002084fc33efe9684c9d95f588bec2b
Guga Villar <grsv.21@gmail.com>
2022-12-24 19:12:34 -0300

    :sparkles: feat: inputs formatters celPhone and Phone

---
+++ src/common/formatters/inputs.ts
@@ -0,0 +1,19 @@
+export const fieldFormatCelPhone = (value: string) => {
+  if (!value) return
+  return value
+    ?.replace(/\D/g, '')
+    .replace(/^(\d)/, '($1')
+    .replace(/^\((\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{5})(\d{1,4})/, '$1-$2')
+    .replace(/(-\d{4})\d+?$/, '$1')
+}
+
+export const fieldFormatPhone = (value: string) => {
+  if (!value) return
+  return value
+    ?.replace(/\D/g, '')
+    .replace(/^(\d)/, '($1')
+    .replace(/^\((\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{4})(\d{1,4})/, '$1-$2')
+    .replace(/(-\d{4})\d+?$/, '$1')
+}
