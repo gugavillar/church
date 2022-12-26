97af19df252858d8712878725bbdf607527a4cf1
Guga Villar <grsv.21@gmail.com>
2022-12-26 16:27:30 -0300

    :sparkles: feat: add new item in menu

--- src/common/components/Navbar/constants.ts
+++ src/common/components/Navbar/constants.ts
@@ -1,5 +1,6 @@
-export const itensMenu = [
+export const ITENS_MENU = [
   { label: 'Home', url: '/' },
   { label: 'About us', url: '/sobre-nos' },
-  { label: 'Sermon', url: '/sermoes' }
+  { label: 'Sermon', url: '/sermoes' },
+  { label: 'Cursilho', url: '/cursilho' }
 ] as const
