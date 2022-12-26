355767c77e0e31c94982725d137497801d3002ca
Guga Villar <grsv.21@gmail.com>
2022-12-26 20:42:54 -0300

    :sparkles: feat: create cursillo page

---
+++ src/modules/visitors/events/Cursillo.tsx
@@ -0,0 +1,34 @@
+import { Fragment } from 'react'
+
+import { Heading, SimpleGrid, Box } from '@chakra-ui/react'
+
+import { CursilloLinkCards } from '@visitors/components'
+
+const Cursillo = () => {
+  return (
+    <Fragment>
+      <Box mb={8}>
+        <Heading mb={6}>Cursilho Feminino</Heading>
+        <SimpleGrid
+          minChildWidth={64}
+          spacing={8}
+          justifyItems='center'
+        >
+          <CursilloLinkCards gender='feminino' />
+        </SimpleGrid>
+      </Box>
+      <Box>
+        <Heading mb={6}>Cursilho Masculino</Heading>
+        <SimpleGrid
+          minChildWidth={64}
+          spacing={8}
+          justifyItems='center'
+        >
+          <CursilloLinkCards gender='masculino' />
+        </SimpleGrid>
+      </Box>
+    </Fragment>
+  )
+}
+
+export default Cursillo
