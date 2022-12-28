abb1714021884852ee89a0b1f058278703be4dde
Guga Villar <grsv.21@gmail.com>
2022-12-28 17:46:44 -0300

    :sparkles: feat: select constants to marital status and brazilian states

---
+++ src/common/constants/selects.ts
@@ -0,0 +1,37 @@
+export const MARITAL_STATUS = [
+  { label: 'Solteiro(a)', value: 'Solteiro(a)' },
+  { label: 'Casado(a)', value: 'Casado(a)' },
+  { label: 'Separado(a)', value: 'Separado(a)' },
+  { label: 'Divorciado(a)', value: 'Divorciado(a)' },
+  { label: 'Viúvo(a)', value: 'Viúvo(a)' }
+] as const
+
+export const BRAZILIAN_STATES = [
+  { label: 'AC', value: 'AC' },
+  { label: 'AL', value: 'AL' },
+  { label: 'AP', value: 'AP' },
+  { label: 'AM', value: 'AM' },
+  { label: 'BA', value: 'BA' },
+  { label: 'CE', value: 'CE' },
+  { label: 'DF', value: 'DF' },
+  { label: 'ES', value: 'ES' },
+  { label: 'GO', value: 'GO' },
+  { label: 'MA', value: 'MA' },
+  { label: 'MT', value: 'MT' },
+  { label: 'MS', value: 'MS' },
+  { label: 'MG', value: 'MG' },
+  { label: 'PA', value: 'PA' },
+  { label: 'PB', value: 'PB' },
+  { label: 'PR', value: 'PR' },
+  { label: 'PE', value: 'PE' },
+  { label: 'PI', value: 'PI' },
+  { label: 'RJ', value: 'RJ' },
+  { label: 'RN', value: 'RN' },
+  { label: 'RS', value: 'RS' },
+  { label: 'RO', value: 'RO' },
+  { label: 'RR', value: 'RR' },
+  { label: 'SC', value: 'SC' },
+  { label: 'SP', value: 'SP' },
+  { label: 'SE', value: 'SE' },
+  { label: 'TO', value: 'TO' }
+] as const
