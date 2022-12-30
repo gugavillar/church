67a8eff9732318cd4f9c0b8f3d5a423e4bb319ae
Guga Villar <grsv.21@gmail.com>
2022-12-30 10:03:48 -0300

    :bug: fix: customValidatePhone to recive phoneType

--- src/common/validations/customFunctions.ts
+++ src/common/validations/customFunctions.ts
@@ -3,10 +3,14 @@ import { validateCPF } from 'validations-br'
 
 import { validateZipCode } from '@common/services/validationsServices'
 
-export const customValidatePhone = (phone: string) => {
+export const customValidatePhone = (phone: string, phoneType: 'celPhone' | 'phone' | 'both') => {
   if (!phone) return false
-  const regex = /^([1-9]{2})(?:[1-9])(\d{3,4})(\d{4})$/g
-  return regex.test(String(phone))
+  const regexCelPhone = /^([1-9]{2})(?:[1-9])(\d{4})(\d{4})$/g
+  const regexPhone = /^([1-9]{2})(?:[1-9])(\d{3})(\d{4})$/g
+  if (phoneType === 'both') {
+    return regexCelPhone.test(String(phone)) || regexPhone.test(String(phone))
+  }
+  return phoneType === 'celPhone' ? regexCelPhone.test(String(phone)) : regexPhone.test(String(phone))
 }
 
 export const customValidateTaxpayer = (cpf: string) => {
