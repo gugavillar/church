40fe920a28f19d5e2a86059c4f16c18cf69fb406
Guga Villar <grsv.21@gmail.com>
2023-01-02 05:08:34 -0300

    :sparkles: feat: useStep hook

---
+++ src/common/hooks/useSteps.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+type StepsHookProps = {
+  stepInitial: number
+  stepLength: number
+}
+
+export const useSteps = ({ stepInitial, stepLength }: StepsHookProps) => {
+  const [activeStep, setActiveStep] = useState(stepInitial)
+
+  const nextStep = () => {
+    setActiveStep((currentStep) => {
+      if (currentStep === stepLength) return currentStep
+
+      return currentStep + 1
+    })
+  }
+
+  const prevStep = () => {
+    setActiveStep((currentStep) => {
+      if (currentStep === 0) return currentStep
+
+      return currentStep - 1
+    })
+  }
+
+  return { nextStep, prevStep, activeStep }
+}
