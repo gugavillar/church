2fd9b2f8635ec8027c53782bdbc9d600cbcd668e
Guga Villar <grsv.21@gmail.com>
2023-01-15 06:40:39 -0300

    :sparkles: feat: create and update cursilhists services
    create cursilhistStripeId and getCursilhistStripeId, rename paymentConfirmation

--- src/common/services/cursilhistsServices.ts
+++ src/common/services/cursilhistsServices.ts
@@ -11,3 +11,34 @@ export const createCursilhist = (cursilhist: Cursilhist) =>
 
 export const getCursilhist = (ref: string) =>
   faunaAPI.query<CursilhistDatabase>(faunaQ.Get(faunaQ.Ref(faunaQ.Collection('cursilhists'), ref)))
+
+type CreatePaymentConfirmationParams = {
+  method: 'pix' | 'money' | 'credit'
+  cursilhistRef: string
+  status: boolean
+  gender: 'masculino' | 'feminino'
+}
+
+export const createCursilhistPaymentConfirmation = (payment: CreatePaymentConfirmationParams) =>
+  faunaAPI.query(
+    faunaQ.Create(faunaQ.Collection('payments'), {
+      data: {
+        ...payment,
+        cursilhistRef: faunaQ.Ref(faunaQ.Collection('cursilhists'), payment.cursilhistRef)
+      }
+    })
+  )
+
+export const createCursilhistStripeId = (ref: string, stripe_id: string) =>
+  faunaAPI.query(
+    faunaQ.Update(faunaQ.Ref(faunaQ.Collection('cursilhists'), ref), {
+      data: {
+        stripe_id
+      }
+    })
+  )
+
+export const getCursilhistStripeId = (email: string) =>
+  faunaAPI.query<CursilhistDatabase>(
+    faunaQ.Get(faunaQ.Match(faunaQ.Index('cursilhist_by_email'), faunaQ.Casefold(email)))
+  )
