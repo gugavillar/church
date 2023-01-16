b79e9777577b9481fbb893783e47ceadc92166aa
Guga Villar <grsv.21@gmail.com>
2023-01-16 06:08:08 -0300

    :sparkles: feat: create useDatabaseAccessError

---
+++ src/common/hooks/useDatabaseAccessError.ts
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+import { useToast } from '@chakra-ui/react'
+
+import { ERROR_TOAST } from '@common/constants'
+
+type UseErrorParams = {
+  key: string
+  description: string
+}
+
+export const useDatabaseAccessError = ({ key, description }: UseErrorParams) => {
+  const toast = useToast()
+  const { push, query, pathname } = useRouter()
+
+  useEffect(() => {
+    if (query[key]) {
+      toast({
+        ...ERROR_TOAST,
+        title: 'Ocorreu uma falha',
+        description
+      })
+      push(pathname, undefined, { shallow: true })
+    }
+  }, [description, key, pathname, push, query, toast])
+}
