aab70ba47d8e37c701ae97dc4088d6e0a59d1f57
Guga Villar <grsv.21@gmail.com>
2023-01-17 19:33:17 -0300

    :sparkles: feat: configurationsErrorParams and useDatabaseError hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
 import { Heading } from '@chakra-ui/react'
 
+import { useDatabaseAccessError } from '@common/hooks'
+
+const configurationsErrorParams = {
+  key: 'configurations',
+  description: 'Falha ao carregar as configurações dos eventos'
+}
+
 const Home = () => {
+  useDatabaseAccessError({ ...configurationsErrorParams })
   return <Heading>Home</Heading>
 }
 
