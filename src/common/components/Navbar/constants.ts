d01ec349678e0f445ac44192c7ff6b8737a943fe
Guga Villar <grsv.21@gmail.com>
2023-01-22 06:54:36 -0300

    :sparkles: feat: remove unused menu items

--- src/common/components/Navbar/constants.ts
+++ src/common/components/Navbar/constants.ts
@@ -1,8 +1,4 @@
-export const ITENS_MENU = [
-  { label: 'Home', url: '/' },
-  { label: 'About us', url: '/sobre-nos' },
-  { label: 'Sermon', url: '/sermoes' }
-] as const
+export const ITENS_MENU = [{ label: 'Home', url: '/' }] as const
 
 export const DROPDOWN_MENU_ITENS = [
   { label: 'Cursilho', url: '/eventos/cursilho' },
