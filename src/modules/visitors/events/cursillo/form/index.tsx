38f5ad534dd7ae8dae9a4b832348d914b18d4432
Guga Villar <grsv.21@gmail.com>
2023-02-09 14:20:33 -0300

    :recycle: refact: using form component

--- src/modules/visitors/events/cursillo/form/index.tsx
+++ src/modules/visitors/events/cursillo/form/index.tsx
@@ -1,134 +1,39 @@
 import { useRouter } from 'next/router'
-import { useEffect, useMemo, useReducer } from 'react'
+import { useEffect, useMemo } from 'react'
 
 import { Box } from '@chakra-ui/react'
 
 import { Steps, PageTitle } from '@common/components'
 
-import { Gender, PaymentMethods } from '@common/@types'
-import { ACTUAL_YEAR, BRAZILIAN_STATES, EDUCATION_LEVEL, MARITAL_STATUS, OCCUPATIONS } from '@common/constants'
+import { Gender } from '@common/@types'
+import { ACTUAL_YEAR } from '@common/constants'
 import { useSteps } from '@common/hooks'
 
 import { ConcludedSubscription } from './components/ConcludedSubscription'
-import { CursilloFormSubscription, defaultFormValues } from './components/FormSubscription'
+import { Form, NewCursilhistForm } from './components/Form'
+import { CursilloFormSubscription } from './components/FormSubscription'
 import { Instructions } from './components/Instructions'
-import { PaymentData } from './components/PaymentData'
-import { ReviewData } from './components/ReviewData'
+import { PaymentData } from './components/PaymentSubscription'
+import { ReviewData } from './components/ReviewSubscription'
 
-export type NewCursilhistForm = {
-  id?: string
-  name: string
-  likeToBeCalled: string
-  birthDate: string
-  phone: string
-  email: string
-  maritalStatus: typeof MARITAL_STATUS[number]['value'] | undefined
-  zipCode: string
-  street: string
-  number: string
-  neighborhood: string
-  city: string
-  state: typeof BRAZILIAN_STATES[number]['value'] | undefined
-  referencePoint?: string
-  spouse?: {
-    name: string
-    phone: string
-    numberOfChildren: number | null
-  }
-  closeRelative?: {
-    name: string
-    phone: string
-  }
-  religion: string
-  church: string
-  education?: typeof EDUCATION_LEVEL[number]['value']
-  occupation?: typeof OCCUPATIONS[number]['value']
-  workplace?: string
-  workplacePhone?: string
-  hasHealthProblems?: '1' | '0'
-  healthProblems?: string
-  hasDietOrFoodRestriction?: '1' | '0'
-  dietOrFoodRestriction?: string
-  wish: string
-  paymentMethod?: PaymentMethods
-}
-
-export type CursilhistStateReducer = {
-  stepProgress: 'formSubscription' | 'reviewSubscription' | 'paymentSubscription' | 'confirmSubscription'
-} & NewCursilhistForm
-
-export type CursilhistActionReducer = {
-  type?: 'formStep' | 'reviewStep' | 'paymentStep' | 'confirmStep'
-  data: CursilhistStateReducer
-}
+type CursilhistStepProgress = 'formSubscription' | 'reviewSubscription' | 'paymentSubscription' | 'confirmSubscription'
 
 type NewCursilhistProps = {
   cursilhist: NewCursilhistForm
-  stepProgress: CursilhistStateReducer['stepProgress']
-}
-
-const reducer = (state: CursilhistStateReducer, action: CursilhistActionReducer) => {
-  switch (action.type) {
-    case 'formStep':
-      return {
-        ...action.data
-      }
-    case 'reviewStep':
-      return {
-        ...state,
-        stepProgress: action.data.stepProgress,
-        id: action.data.id
-      }
-    case 'paymentStep':
-      return {
-        ...state,
-        stepProgress: action.data.stepProgress,
-        paymentMethod: action.data.paymentMethod
-      }
-    case 'confirmStep':
-      return {
-        ...action.data,
-        stepProgress: action.data.stepProgress
-      }
-    default:
-      return {
-        ...state,
-        stepProgress: action.data.stepProgress
-      }
-  }
+  stepProgress: CursilhistStepProgress
 }
 
 const NewCursilhist = ({ cursilhist, stepProgress }: NewCursilhistProps) => {
   const { query } = useRouter()
-  const { activeStep, setActiveStep } = useSteps({ stepInitial: 0, stepLength: 3 })
-
-  const [state, dispatch] = useReducer(reducer, {
-    ...defaultFormValues,
-    stepProgress
-  })
+  const { activeStep, setActiveStep, prevStep, nextStep } = useSteps({ stepInitial: 0, stepLength: 3 })
 
   useEffect(() => {
-    switch (state?.stepProgress) {
-      case 'formSubscription':
-        return setActiveStep(0)
-      case 'reviewSubscription':
-        return setActiveStep(1)
-      case 'paymentSubscription':
-        return setActiveStep(2)
-      case 'confirmSubscription':
-        return setActiveStep(3)
-      default:
-        return setActiveStep(0)
+    if (stepProgress === 'reviewSubscription') {
+      setActiveStep(1)
+    } else {
+      setActiveStep(0)
     }
-  }, [setActiveStep, state?.stepProgress])
-
-  useEffect(() => {
-    cursilhist &&
-      dispatch({
-        type: 'formStep',
-        data: { ...cursilhist, stepProgress }
-      })
-  }, [cursilhist, query.success, stepProgress])
+  }, [setActiveStep, stepProgress])
 
   const steps = useMemo(() => {
     return [
@@ -136,8 +41,7 @@ const NewCursilhist = ({ cursilhist, stepProgress }: NewCursilhistProps) => {
         content: (
           <CursilloFormSubscription
             gender={query.gender as Gender}
-            dispatch={dispatch}
-            reducerState={state}
+            handleNextStep={nextStep}
           />
         )
       },
@@ -145,24 +49,24 @@ const NewCursilhist = ({ cursilhist, stepProgress }: NewCursilhistProps) => {
         content: (
           <ReviewData
             gender={query.gender as Gender}
-            dispatch={dispatch}
-            reducerState={state}
+            handlePrevStep={prevStep}
+            handleNextStep={nextStep}
           />
         )
       },
       {
         content: (
           <PaymentData
-            reducerState={state}
-            dispatch={dispatch}
+            handlePrevStep={prevStep}
+            handleNextStep={nextStep}
           />
         )
       },
       {
-        content: <ConcludedSubscription reducerState={state} />
+        content: <ConcludedSubscription />
       }
     ]
-  }, [query.gender, state])
+  }, [nextStep, prevStep, query.gender])
 
   return (
     <Box>
@@ -170,10 +74,12 @@ const NewCursilhist = ({ cursilhist, stepProgress }: NewCursilhistProps) => {
         Cursilho {query.gender} de cristandade vida - {ACTUAL_YEAR}
       </PageTitle>
       <Instructions />
-      <Steps
-        activeStep={activeStep}
-        steps={steps}
-      />
+      <Form cursilhist={cursilhist}>
+        <Steps
+          activeStep={activeStep}
+          steps={steps}
+        />
+      </Form>
     </Box>
   )
 }
