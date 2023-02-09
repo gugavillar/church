1e3db4de8cf3c11642d0120189b1aaba17982406
Guga Villar <grsv.21@gmail.com>
2023-02-09 13:15:18 -0300

    :sparkles: feat: create wishdata component

---
+++ src/modules/visitors/events/cursillo/form/components/fields/WishData.tsx
@@ -0,0 +1,24 @@
+import { useFormContext } from 'react-hook-form'
+
+import { Textarea } from '@chakra-ui/react'
+
+import { FieldController } from '@common/components'
+
+import { NewCursilhistForm } from '../..'
+
+export const WishData = () => {
+  const {
+    register,
+    formState: { errors }
+  } = useFormContext<NewCursilhistForm>()
+  return (
+    <FieldController
+      error={errors?.wish?.message as string}
+      label='Porque deseja fazer o cursilho'
+      mt={6}
+      isRequired
+    >
+      <Textarea {...register('wish')} />
+    </FieldController>
+  )
+}
